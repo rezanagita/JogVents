b3ce52e6d62b80472ef282737a797a73626f371a
REZA NAGITA <rezagitta@com>
2024-07-18 16:20:59 +0700

    add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/firebase_options.dart';
 import 'login_page.dart';
